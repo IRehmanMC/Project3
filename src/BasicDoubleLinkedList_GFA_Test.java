645dd6357a4ab81e632a6e6324607e78a2aab29b
IRehmanMC <117783639+IRehmanMC@users.noreply.github.com>
2024-03-10 20:00:13 -0400

    Add files via upload

---
+++ src/BasicDoubleLinkedList_GFA_Test.java
@@ -0,0 +1,41 @@
+
+/**
+ * GAF Test class for BasicDoubleLinkedList
+ * 
+ * @author Farnaz Eivazi
+ * @version 1/29/2024
+ */
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BasicDoubleLinkedList_GFA_Test {
+	BasicDoubleLinkedList<String> linkedString;
+	BasicDoubleLinkedList<Double> linkedDouble;
+
+
+	@Before
+	public void setUp() throws Exception {
+		linkedString = new BasicDoubleLinkedList<>();
+		linkedString.addToEnd("Hello");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		linkedString = null;
+	}
+
+	@Test
+	public void testGetSize() {
+		assertEquals(1,linkedString.getSize());
+	}	
+}
